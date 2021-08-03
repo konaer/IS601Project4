8ce4d42deeae0826539f6b68c54e499c3dda238d
Dejing Kong <dejingk@gmail.com>
2021-08-02 23:02:04 -0400

    task: creating first flask application

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+# Create Flask's `app` object
+app = Flask(
+    __name__,
+    template_folder="templates"
+)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
